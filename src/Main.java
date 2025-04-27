4b33b760924e7213de221694ed1904f0cb1bcfe3
cagla <cagla_iinan@outlook.com>
2025-04-27 14:43:09 +0300

    1. Hafta Ödevi- Sayı Tahmin Oyunu

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,31 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        System.out.println("Sayı Tahmin Oyununa Hoş Geldiniz!");
+
+        int rastgeleSayi = (int)(Math.random()*100+1);
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Tahmin Ettiginiz Sayiyi Yazin:");
+            int tahminEdilenSayi = scanner.nextInt();
+
+            if (tahminEdilenSayi == rastgeleSayi) {
+                System.out.println("Tebrikler! Doğru tahmin.");
+                break;
+            } else if (tahminEdilenSayi > rastgeleSayi) {
+                System.out.println("Daha küçük bir sayı girin.");
+            } else {
+                System.out.println("Daha büyük bir sayı girin.");
+            }
         }
+
+        System.out.println("Oyun bitti!");
+
     }
 }
